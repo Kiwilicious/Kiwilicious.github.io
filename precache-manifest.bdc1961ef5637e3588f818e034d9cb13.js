40515ecda853729485b3ba26188036335ea8eaa3
James Hwang <jhwang137@gmail.com>
2019-08-16 15:08:01 -0500

    Updates

---
+++ precache-manifest.bdc1961ef5637e3588f818e034d9cb13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d1e298053ccafa2d18ccca959506735",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5f1e466c1b9b4d1780f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f65a2cc772cc6a83dec0",
+    "url": "/static/js/2.18914a5b.chunk.js"
+  },
+  {
+    "revision": "a5f1e466c1b9b4d1780f",
+    "url": "/static/js/main.cfb93b14.chunk.js"
+  },
+  {
+    "revision": "9aed3f29f8edb7f07a66",
+    "url": "/static/js/runtime~main.a96ab039.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
