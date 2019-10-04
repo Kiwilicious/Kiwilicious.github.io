6e99851380538d22af0f27a27a37edabacbe3f01
James Hwang <jhwang137@gmail.com>
2019-10-04 16:55:30 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import styled from "styled-components";
 import Card from "./components/Card";
 import Navbar from "./components/Navbar/Navbar";
 
+import EmailIcon from "./assets/email.svg";
+import GithubIcon from "./assets/github.svg";
+import LinkedinIcon from "./assets/linkedin.svg";
+
 const AppContainer = styled.div`
   margin: 0 auto;
   width: 1000px;
 `;
 
 const AppContent = styled.div`
-  margin: 0 auto;
+  margin: 50px auto 0;
   width: 800px;
 `;
 
@@ -18,10 +22,26 @@ const SectionContainer = styled.div`
   line-height: 2em;
 `;
 
+const SectionTitle = styled.h3`
+  font-weight: 500;
+  text-transform: uppercase;
+`;
+
 const TestCardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-around;
+`;
+
+const ContactIconsContainer = styled.div`
+  display: flex;
+  justify-content: left;
+`;
+
+const ContactIcon = styled.img`
+  margin-left: 20px;
+  width: 50px;
+  height: 50px;
 `;
 
 const App: React.FC = () => {
@@ -30,40 +50,18 @@ const App: React.FC = () => {
       <Navbar />
       <AppContent>
         <SectionContainer id="home">
-          Leverage agile frameworks to provide a robust synopsis for high level
-          overviews. Iterative approaches to corporate strategy foster
-          collaborative thinking to further the overall value proposition.
-          Organically grow the holistic world view of disruptive innovation via
-          workplace diversity and empowerment. Bring to the table win-win
-          survival strategies to ensure proactive domination. At the end of the
-          day, going forward, a new normal that has evolved from generation X is
-          on the runway heading towards a streamlined cloud solution. User
-          generated content in real-time will have multiple touchpoints for
-          offshoring.
+          <h1>James Hwang</h1>
+          <h2>FullStack Web Developer</h2>
+          <hr />
+          <p>
+            This is a simple site made in React and hosted on Github where you
+            can also check out the source code. Contains some of my personal
+            projects and code snippets. Feel free to reach out to me using the
+            contacts posted below.
+          </p>
         </SectionContainer>
         <SectionContainer id="projects">
-          Capitalize on low hanging fruit to identify a ballpark value added
-          activity to beta test. Override the digital divide with additional
-          clickthroughs from DevOps. Nanotechnology immersion along the
-          information highway will close the loop on focusing solely on the
-          bottom line. Podcasting operational change management inside of
-          workflows to establish a framework. Taking seamless key performance
-          indicators offline to maximise the long tail. Keeping your eye on the
-          ball while performing a deep dive on the start-up mentality to derive
-          convergence on cross-platform integration.
-        </SectionContainer>
-        <SectionContainer id="test">
-          Collaboratively administrate empowered markets via plug-and-play
-          networks. Dynamically procrastinate B2C users after installed base
-          benefits. Dramatically visualize customer directed convergence without
-          revolutionary ROI. Efficiently unleash cross-media information without
-          cross-media value. Quickly maximize timely deliverables for real-time
-          schemas. Dramatically maintain clicks-and-mortar solutions without
-          functional solutions. Completely synergize resource taxing
-          relationships via premier niche markets. Professionally cultivate
-          one-to-one customer service with robust ideas. Dynamically innovate
-          resource-leveling customer service for state of the art customer
-          service.
+          <SectionTitle>Projects</SectionTitle>
           <TestCardContainer>
             {new Array(10).fill(0).map((_, i) => (
               <div style={{ margin: "20px 0" }}>
@@ -82,18 +80,14 @@ const App: React.FC = () => {
           </TestCardContainer>
         </SectionContainer>
         <SectionContainer id="contact">
-          Objectively innovate empowered manufactured products whereas parallel
-          platforms. Holisticly predominate extensible testing procedures for
-          reliable supply chains. Dramatically engage top-line web services
-          vis-a-vis cutting-edge deliverables. Proactively envisioned multimedia
-          based expertise and cross-media growth strategies. Seamlessly
-          visualize quality intellectual capital without superior collaboration
-          and idea-sharing. Holistically pontificate installed base portals
-          after maintainable products. Phosfluorescently engage worldwide
-          methodologies with web-enabled technology. Interactively coordinate
-          proactive e-commerce via process-centric "outside the box" thinking.
-          Completely pursue scalable customer service through sustainable
-          potentialities.
+          <SectionTitle>Contact</SectionTitle>
+          You can reach me via email and LinkedIn as well as view my profile
+          over at Github.
+          <ContactIconsContainer>
+            <ContactIcon src={EmailIcon} alt="email-icon"></ContactIcon>
+            <ContactIcon src={LinkedinIcon} alt="linkedin-icon"></ContactIcon>
+            <ContactIcon src={GithubIcon} alt="github-icon"></ContactIcon>
+          </ContactIconsContainer>
         </SectionContainer>
       </AppContent>
     </AppContainer>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,9 +4,15 @@ import { Link } from "react-scroll";
 
 import styles from "./Navbar.module.css";
 
-const NavContainer = styled.nav`
+const Fadeaway = styled.div`
+  background: linear-gradient(to top, transparent, white);
+  height: 30px;
+`;
+
+const NavLinks = styled.nav`
   align-items: center;
   background-color: white;
+  /* box-shadow: 0 20px 10px white; */
   display: flex;
   height: 70px;
   position: sticky;
@@ -17,41 +23,44 @@ const NavContainer = styled.nav`
 
 const Navbar = () => {
   return (
-    <NavContainer>
-      <Link
-        activeClass={styles.active}
-        className={styles["nav-link"]}
-        duration={500}
-        offset={-70}
-        smooth={true}
-        spy={true}
-        to="home"
-      >
-        Home
-      </Link>
-      <Link
-        activeClass={styles.active}
-        className={styles["nav-link"]}
-        duration={500}
-        offset={-70}
-        smooth={true}
-        spy={true}
-        to="projects"
-      >
-        Projects
-      </Link>
-      <Link
-        activeClass={styles.active}
-        className={styles["nav-link"]}
-        duration={500}
-        offset={-70}
-        smooth={true}
-        spy={true}
-        to="contact"
-      >
-        Contact
-      </Link>
-    </NavContainer>
+    <div>
+      <NavLinks>
+        <Link
+          activeClass={styles.active}
+          className={styles["nav-link"]}
+          duration={500}
+          offset={-70}
+          smooth={true}
+          spy={true}
+          to="home"
+        >
+          Home
+        </Link>
+        <Link
+          activeClass={styles.active}
+          className={styles["nav-link"]}
+          duration={500}
+          offset={-70}
+          smooth={true}
+          spy={true}
+          to="projects"
+        >
+          Projects
+        </Link>
+        <Link
+          activeClass={styles.active}
+          className={styles["nav-link"]}
+          duration={500}
+          offset={-70}
+          smooth={true}
+          spy={true}
+          to="contact"
+        >
+          Contact
+        </Link>
+      </NavLinks>
+      <Fadeaway></Fadeaway>
+    </div>
   );
 };
 
